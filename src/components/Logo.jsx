243631fdff9e11c8792649d67f9fb56cd6a22ab5
user.walid1921 <user.kouiderayadwalid@gmail.com>
2023-10-27 01:13:49 +0300

    newDesign

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 function Logo() {
   return (
     <div className="logo">
-    <span role="img">🍿</span>
-    <h1>usePopcorn</h1>
+    <span role="img">🎬</span>
+    <h1>iMovie</h1>
   </div>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,17 +2,10 @@
 
 function MovieList({ movies, handleSelectedMovie }) {
   return (
-    <ul className="list list-movies">
+    <ul className="list list-movies flex justify-center flex-wrap gap-2 my-14">
       {movies?.map((movie) => (
         <li key={movie.imdbID} onClick={() => handleSelectedMovie(movie.imdbID)}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} className="h-[300px] w-[200px] rounded-xl hover:opacity-50 transition-all ease-in-out duration-500 "/>
         </li>
       ))}
     </ul>
